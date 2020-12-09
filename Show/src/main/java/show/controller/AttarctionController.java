6583421adc81b7937a28db7d23170535b2498260
KimYoungJae <uni0125@naver.com>
2020-12-10 07:24:20 +0900

    ignore test

--- Show/src/main/java/show/controller/AttarctionController.java
+++ Show/src/main/java/show/controller/AttarctionController.java
@@ -36,6 +36,7 @@ public class AttarctionController {
 		System.out.println("2020-12-09 01:44 Merge Test");
 		System.out.println("2020-12-09 09:53 Commit Test");
 		System.out.println("2020-12-09 09:53 Commit Test");
+		System.out.println("Ignore Test");
 	}
 	
 	
