3997cfb66c2b430b5bf15ac61ee5e0153a86d6c0
KimYoungJae <uni0125@naver.com>
2020-12-15 04:16:25 +0900

    test

--- Show/src/main/java/show/controller/AttarctionController.java
+++ Show/src/main/java/show/controller/AttarctionController.java
@@ -46,6 +46,7 @@ public class AttarctionController {
 		System.out.println("2020-12-15 PullrequestTest2");
 		System.out.println("2020-12-15 PullrequestTest3");
 		System.out.println("2020-12-15 PullrequestTest4");
+		System.out.println("2020-12-15 PullrequestTest5");
 	}
 	
 	
